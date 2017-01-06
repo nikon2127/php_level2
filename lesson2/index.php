5b55ef7685897ae15703f14ef99af5738cbd64ab
nikon2127 <nikon2127@gmail.com>
2017-01-06 21:02:49 +0300

    создал lesson2

---
+++ lesson2/index.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+/**
+ * Created by PhpStorm.
+ * User: Никон
+ * Date: 06.01.2017
+ * Time: 20:55
+ */
---
+++ lesson2/view/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Никон
+ * Date: 06.01.2017
+ * Time: 20:56
+ */
