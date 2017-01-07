68aada6bc938b1ddb672c25763d9e89cd870d581
nikon2127 <nikon2127@gmail.com>
2017-01-08 01:32:28 +0300

    добавил в класс sqlLesson2 функции добавления редактированияя удаления и запросы

--- lesson2/model/SqlLesson2.php
+++ lesson2/model/SqlLesson2.php
@@ -14,6 +14,15 @@ class SqlLesson2
     private $password;
     private $bd;
 
+    //приводим данные из формы в нормальную форму
+    public function clean_text($string){
+        $string = trim($string);
+        $string = stripslashes($string);
+        $string = strip_tags($string);
+        $string = htmlspecialchars($string);
+        return $string;
+    }
+
     public function __construct()
     {
         $this->server = 'localhost';
@@ -22,32 +31,48 @@ class SqlLesson2
         $this->bd = 'lesson1';
     }
 
-    //подключаемся к базу данных
-    private function sql_connect(){
-        return mysqli_connect($this->server, $this->user, $this->password, $this->bd);
+    //подключаемся к базе данных
+    private function sql_connect($sql){
+        $link = mysqli_connect($this->server, $this->user, $this->password, $this->bd);
+        if($link){
+            $result = mysqli_query($link, $sql);
+        }
+        mysqli_close($link);
+        if($result){
+            return $result;
+        }
+        return false;
     }
 
-    //закрываем соединение с базой данных
-    private function sql_close($link){
-        return mysqli_close($link);
+    //запрашиваем количество записей в базе
+    public function sql_count(){
+        $sql = "SELECT count(*) FROM `news` WHERE 1";
+        $result = $this->sql_connect($sql);
+        if($result){
+            $row = mysqli_fetch_assoc($result);
+            foreach ($row as $vol){
+                $res = $vol;
+            }
+            $res = (int) $res;
+        }
+        if($res){
+            return $res;
+        }
+        return false;
     }
 
     //запрашиваем все записи у базы
     public function sql_select_all($pos, $col){
         $pos = (int) $pos;
         $col = (int) $col;
-        $link = $this->sql_connect();
-        if($link){
-            $sql = "SELECT * FROM `news` WHERE 1 LIMIT $pos, $col";
-            $result = mysqli_query($link, $sql);
-            if($result){
-                $emps = array();
-                while ($row = mysqli_fetch_assoc($result)){
-                    $emps[] = $row;
-                }
+        $sql = "SELECT * FROM `news` WHERE 1 LIMIT $pos, $col";
+        $result = $this->sql_connect($sql);
+        if($result){
+            $emps = array();
+            while ($row = mysqli_fetch_assoc($result)){
+                $emps[] = $row;
             }
         }
-        $this->sql_close($link);
         if(!empty($emps)){
             return $emps;
         }
@@ -57,16 +82,11 @@ class SqlLesson2
     //запрашиваем одну запись по id
     public function sql_select_one($id){
         $id = (int) $id;
-        $link = $this->sql_connect();
-        if($link){
-            $sql = "SELECT * FROM `news` WHERE `id_news` = '$id'";
-            $result = mysqli_query($link, $sql);
-            if($result){
-                $row = mysqli_fetch_assoc($result);
-
-            }
+        $sql = "SELECT * FROM `news` WHERE `id_news` = $id";
+        $result = $this->sql_connect($sql);
+        if($result){
+            $row = mysqli_fetch_assoc($result);
         }
-        $this->sql_close($link);
         if(!empty($row)){
             return $row;
         }
@@ -74,17 +94,26 @@ class SqlLesson2
     }
 
     //редактируем запись
-    public function sql_edit(){
-
+    public function sql_edit($id, $title, $text){
+        $id = (int) $id;
+        $title = $this->clean_text($title);
+        $text = $this->clean_text($text);
+        $sql = "UPDATE `news` SET `header` = '$title',`text` = '$text' WHERE `id_news` = $id";
+        return $this->sql_connect($sql);
     }
 
     //добавляем запись
-    public function sql_add(){
-
+    public function sql_add($title, $text){
+        $title = $this->clean_text($title);
+        $text = $this->clean_text($text);
+        $sql = "INSERT INTO `news`(`header`, `text`) VALUES ('$title', '$text')";
+        return $this->sql_connect($sql);
     }
 
     //удаляем запись
     public function sql_delete($id){
-
+        $id = (int) $id;
+        $sql = "DELETE FROM `news` WHERE `id_news` = $id";
+        return $this->sql_connect($sql);
     }
 }
