acae56602f4fd8b35bc1d05a925da2d37b24c4eb
nikon2127 <nikon2127@gmail.com>
2017-01-11 00:08:11 +0300

    добавил абстрактный класс

--- lesson3/function/ArticleAbstract.php
+++ lesson3/function/ArticleAbstract.php
@@ -3,4 +3,18 @@
 abstract class ArticleAbstract
 {
 
+    protected static $table;
+    protected static $class;
+
+    public static function getAll(){
+        $all = new Sql();
+        $sql = "SELECT * FROM " . static::$table;
+        return $all->select($sql, static::$class);
+    }
+
+    public static function getOne($id){
+        $one = new Sql();
+        $sql = "SELECT * FROM ". static::$table. " WHERE `id_news` = $id";
+        return $one->selectOne($sql, static::$class);
+    }
 }
--- lesson3/model/News.php
+++ lesson3/model/News.php
@@ -5,22 +5,8 @@ class News extends ArticleAbstract
     public $id_news;
     public $header;
     public $text;
-    public static $class;
 
-    public function __construct()
-    {
-        $this->class = 'News';
-    }
+    protected static $class = 'News';
+    protected static $table = 'news';
 
-    public static function getAll(){
-        $all = new Sql();
-        $sql = "SELECT * FROM `news`";
-        return $all->select($sql, 'News');
-    }
-
-    public static function getOne($id){
-        $one = new Sql();
-        $sql = "SELECT * FROM `news` WHERE `id_news` = $id";
-        return $one->selectOne($sql, 'News');
-    }
 }
