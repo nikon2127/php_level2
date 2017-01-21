72c246c23850a87541da23c54855467d9767cd0d
nikon2127 <nikon2127@gmail.com>
2017-01-21 19:23:24 +0300

    переделал редактирование новости

--- lesson3/controller/AdminController.php
+++ lesson3/controller/AdminController.php
@@ -2,13 +2,13 @@
 error_reporting(E_ALL);
 class AdminController
 {
-    public function actionAdd(){
+    public function actionAdd()
+    {
 
         if(!empty($_POST['title'])){
             $title = $_POST['title'];
         }else {
             $title = '';
-            $text = '';
         }
 
         if(!empty($_POST['text'])){
@@ -18,7 +18,7 @@ class AdminController
         }
 
         if(!empty($title) && !empty($text)){
-            $news = News::getAdd($title, $text);
+            News::getAdd($title, $text);
             header("Location: index.php");
         }
 
@@ -29,32 +29,52 @@ class AdminController
         //include __DIR__ . '/../view/add.php';
     }
 
-    public function actionDelete(){
+    public function actionDelete()
+    {
         $id = (int) $_GET['id'];
         News::getDelete($id);
         header("Location: index.php");
     }
 
-    public function actionEdit(){
+    public function actionEdit()
+    {
         if(!empty($_GET['id'])){
             $id = (int) $_GET['id'];
             $news = News::getOne($id);
             $title = $news->header;
             $text = $news->text;
+        }else{
+            $id = '';
+            $title = '';
+            $text = '';
         }
+
         if(isset($_POST['title'])){
             $title = $_POST['title'];
         }
+
         if(isset($_POST['text'])){
             $text = $_POST['text'];
         }
+
         if(isset($_POST['id'])){
-            $id = $_POST['id'];
+            $id = (int) $_POST['id'];
         }
+
+        if(empty($id)){
+            header("Location: index.php");
+        }
+
         if(!empty($_POST['title']) && !empty($_POST['text']) && !empty($_POST['id'])){
             News::getEdit($_POST['id'], $_POST['title'], $_POST['text']);
             header("Location: index.php");
         }
-        include __DIR__ . '/../view/edit.php';
+
+        $view = new View();
+        $view->id = $id;
+        $view->title = $title;
+        $view->text = $text;
+        $view->display('edit.php');
+        //include __DIR__ . '/../view/edit.php';
     }
 }
--- lesson3/view/edit.php
+++ lesson3/view/edit.php
@@ -10,9 +10,9 @@ error_reporting(E_ALL);
 <body>
 <form method="post" action="index.php?ctrl=Admin&act=Edit">
     <input type="hidden" name="id" value="<?php echo $id; ?>">
-    <p>заголовок: <input type="text" name="title" <?php if(!empty($title)): ?>value="<?php echo $title; ?>"<?php endif; ?>></p>
+    <p>заголовок: <input type="text" name="title" value="<?php echo $title; ?>"></p>
     <p>текст:<br>
-        <textarea rows="25" cols="45" name="text"><?php if(!empty($text)): echo $text; endif; ?></textarea></p>
+        <textarea rows="25" cols="45" name="text"><?php echo $text; ?></textarea></p>
     <input type="submit">
 </form>
 </body>
