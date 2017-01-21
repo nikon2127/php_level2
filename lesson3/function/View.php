f018e024f0c4a43a1501aa6553a202aa09f62886
nikon2127 <nikon2127@gmail.com>
2017-01-21 18:27:34 +0300

    добавил в классе View функции

--- lesson3/function/View.php
+++ lesson3/function/View.php
@@ -1,6 +1,60 @@
 <?php
 
-class View
+class View implements Iterator
 {
+    protected $data = [];
 
+    //вызывается при создании необьявленой переменной
+    public function __set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    //вызывается при прочтении необьявленой переменной
+    public function __get($key)
+    {
+        return $this->data[$key];
+    }
+
+    public function render($template)
+    {
+        foreach ($this->data as $key => $vol){
+            $$key = $vol;
+        }
+        ob_start();
+        include __DIR__ . '/../view/' . $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    public function display($template)
+    {
+        echo $this->render($template);
+    }
+
+    public function current()
+    {
+
+    }
+
+    public function next()
+    {
+
+    }
+
+    public function key()
+    {
+
+    }
+
+    public function valid()
+    {
+
+    }
+
+    public function rewind()
+    {
+
+    }
 }
