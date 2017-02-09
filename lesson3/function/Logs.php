37d26a5e0be3995b123cbbbeff64d9c603f038f8
nikon2127 <nikon2127@gmail.com>
2017-02-09 22:16:04 +0300

    востановленая версия

---
+++ lesson3/function/Logs.php
@@ -0,0 +1,19 @@
+<?php
+
+class Logs
+    extends Exception
+{
+    public function __construct($message, $code, $file, $line)
+    {
+        $this->message = $message;
+        $this->code = $code;
+        $this->file = $file;
+        $this->line = $line;
+    }
+
+    public function logInsert(){
+        $data = date(DATE_RFC822) . ' | ' . $this->message . ' | ' . $this->code . ' | ' .
+            $this->file . ' | ' . $this->line . "\r";
+        file_put_contents(__DIR__ . '/../log.txt', $data, FILE_APPEND);
+    }
+}
